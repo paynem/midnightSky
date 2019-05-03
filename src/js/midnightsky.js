2b1a4bbaca0f50e56710d7cfd90f14d3570d881e
Michael Payne <mpayne677@gmail.com>
2019-05-03 10:25:30 -0700

    almost finished; need to add comments

--- src/js/midnightsky.js
+++ src/js/midnightsky.js
@@ -93,23 +93,193 @@ import './general.js';
         takes the position of the mouse into account.
   END OF PART 3 - TEST AND DEBUG YOUR CODE - YOU SHOULD SEE CONSTELLATIONS ON YOUR PAGE       
 */
-
+/******
+ * class midnightsky
+ ******/
 class MidnightSky {
 
     constructor() {
+       
+        //this.$canvas = document.querySelector('#imgCanvas');
+        this.$canvas = document.getElementById("imgCanvas");
+        this.$context = this.$canvas.getContext('2d');
+        //this.$animationFrame;
+        this.defaults = {
+            star: {
+                color: 'rgba(255, 255, 255, .5)',
+                width: 3,
+                randomWidth: true
+            },
+            line: {
+                color: 'rgba(255, 255, 255, .5)',
+                width: 0.2
+            },
+            position: {
+                x: 0,
+                y: 0
+            },
+            width: window.innerWidth,
+            height: window.innerHeight,
+            velocity: 0.1,
+            length: 100,
+            distance: 120,
+            radius: 150,
+            stars: []
+        };
+        this.config = JSON.parse(JSON.stringify(this.defaults));
+        this.setCanvas();
+        this.setContext();
+        this.setInitialPosition();
+        this.createStars();
+        this.drawStars();
+        setInterval(this.animateStars.bind(this), 1000/60);
+        document.getElementById("imgCanvas").onmousemove = this.highlight.bind(this);
+    }
+    /******
+    * setCanvas method
+    ******/
+    setCanvas() {
+        this.$canvas.height = this.config.height;
+        this.$canvas.width = this.config.width;
+
+
+    }
+    /******
+    * setContext method
+    ******/
+    setContext() {
+       
+        this.$context.lineWidth  = this.config.star.width;
+        this.$context.strokeStyle = this.config.star.color;
+        this.$context.fillStyle = this.config.star.color;
+    }
+    /******
+    * setInitialPosition method
+    ******/
+    setInitialPosition() {
+        this.config.position.x = this.$canvas.width/2;
+        this.config.position.y = this.$canvas.height/2;
+    }
+    /******
+    * createStar method
+    ******/
+    createStar() {
+        let num = Math.floor(Math.random()*10) + 1; 
+            num *= Math.floor(Math.random()*2) == 1 ? 1 : -1; 
+            let num1 = Math.floor(Math.random()*10) + 1; 
+            num1 *= Math.floor(Math.random()*2) == 1 ? 1 : -1; 
+        let star = {
+            color: this.config.star.color,
+            width: this.config.star.width,
+            randomWidth: this.config.star.randomWidth,
+            x: Math.floor(Math.random() * this.$canvas.width) + 1,
+            y: Math.floor(Math.random() * this.$canvas.height) + 1,
+            vx: num, //Math.random(),
+            vy: num1,//Math.random(),
+            radius: Math.floor(Math.random()*5) + 1
+        }
+        return star;
+
+    }
+    /******
+    * createStars method
+    ******/
+    createStars() {
+        
+        this.createStar = this.createStar.bind(this);
+        
+        
+        for(let i = 0; i < this.config.length; i++ ) {
+            
+            this.config.stars[i] = this.createStar();
+            
+        }
+    }
+    drawStar(star) {
+        
+        this.$context.beginPath();
+        this.$context.moveTo(star.x+star.radius, star.y+star.radius);
+        this.$context.lineTo(star.x-star.radius, star.y-star.radius);
+        /*this.$context.stroke();
+        this.$context.closePath();
+        this.$context.beginPath();*/
+        this.$context.moveTo(star.x-star.radius, star.y+star.radius);
+        this.$context.lineTo(star.x+star.radius, star.y-star.radius);
+        this.$context.stroke();
+        this.$context.closePath();
+        this.$context.lineWidth  = this.config.line.width;
+        this.drawLines = this.drawLines.bind(this);
+        this.drawLines();
+        this.$context.lineWidth  = this.config.star.width;
+
+
+    }
+    drawStars() {
+        this.drawStar = this.drawStar.bind(this);
+        this.$context.clearRect(0, 0, this.config.width, this.config.height);
+        for (let i = 0; i < this.config.length; i++)
+        {
+            this.drawStar(this.config.stars[i]);
+        }
         
     }
+    moveStar(star) {
+        star.x += star.vx;
+        star.y += star.vy;
+        if (star.x <= 0) {
+            star.x = 1;
+            star.vx = Math.floor(Math.random()*10) + 1;
+        }
+        else if (star.x >= this.config.width) {
+            star.x = this.config.width -1;
+            star.vx = (Math.floor(Math.random()*10) + 1) * -1;
+        }
+        if (star.y <= 0) {
+            star.y = 1;
+            star.vy = star.vx = Math.floor(Math.random()*10) + 1;
+        }
+        else if (star.y >= this.config.height) {
+            star.y = this.config.height -1;
+            star.vy = (Math.floor(Math.random()*10) + 1) * -1;
+        }
 
-    /*
+    }
+    moveStars() {
+        this.moveStar = this.moveStar.bind(this);
+        for (let i = 0; i < this.config.length; i++)
+        {
+            this.moveStar(this.config.stars[i]);
+        }
+
+    }
+    animateStars() {
+        this.$context.clearRect(0, 0, this.config.width, this.config.height);
+        this.moveStars = this.moveStars.bind(this);
+        this.moveStars();
+        for (let i = 0; i < this.config.length; i++)
+        {
+            this.drawStar(this.config.stars[i]);
+        }
+    }
+    
     highlight(e) {
-        this.config.position.x = e.pageX - this.$canvas.offsetLeft;
-        this.config.position.y = e.pageY - this.$canvas.offsetTop;
+        this.config.position.x = e.pageX //- this.$canvas.offsetLeft;
+        this.config.position.y = e.pageY //- this.$canvas.offsetTop;
     }
     drawLines () {
         for (let i = 0; i < this.config.length; i++) {
             for (let j = 0; j < this.config.length; j++) {
                 let iStar = this.config.stars[i];
                 let jStar = this.config.stars[j];
+                /*if (((jStar.y - iStar.y)**2 + (jStar.x - iStar.x)**2)**(1/2) < this.config.distance) {
+                    if (((this.config.position.y -iStar.y)**2 + (this.config.position.x - iStar.x)**2)**(1/2) < this.config.radius) {
+                        this.$context.beginPath();
+                        this.$context.moveTo(iStar.x, iStar.y);
+                        this.$context.lineTo(jStar.x, jStar.y);
+                        this.$context.stroke();
+                        this.$context.closePath();
+                    }
+                }*/
                 if ((iStar.x - jStar.x) < this.config.distance &&
                     (iStar.y - jStar.y) < this.config.distance &&
                     (iStar.x - jStar.x) > - this.config.distance &&
@@ -124,11 +294,10 @@ class MidnightSky {
                         this.$context.stroke();
                         this.$context.closePath();
                     }
-                }
+                    }
             }
         }
     }
-    */
 }
 
 let midnightsky;
